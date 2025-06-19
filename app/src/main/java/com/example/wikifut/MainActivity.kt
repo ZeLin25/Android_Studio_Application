3bac1be789da9ce9829f43026b458ce74958270a
joaoo <joao.oliveira2572005@gmail.com>
2025-06-19 15:31:18 +0100

    Testes de Tamanho da Fonte e as margens entre as linhas

--- app/src/main/java/com/example/wikifut/MainActivity.kt
+++ app/src/main/java/com/example/wikifut/MainActivity.kt
@@ -10,7 +10,9 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.wikifut.ui.theme.WikiFutTheme
 
 
@@ -35,7 +37,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingText(message: String, modifier: Modifier = Modifier) {
     Text(
-        text = message
+        text = message,
+        fontSize = 100.sp,
+        lineHeight = 116.sp
     )
 }
 
@@ -43,6 +47,6 @@ fun GreetingText(message: String, modifier: Modifier = Modifier) {
 @Composable
 fun HelloJoaoPreview() {
     WikiFutTheme {
-        GreetingText(message = "Happy Birthdate João!!!")
+        GreetingText(message = "Happy Birthdate João!")
     }
 }
